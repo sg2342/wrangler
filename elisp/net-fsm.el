d55f67b2991112c16fd0bd0d94329b3b93277967
Stefan Grundmann <sg2342@googlemail.com>
2017-11-09 23:34:16 +0000

    import distel fe01b4352199c124d7c91b0af6c5d447f4f218fa

--- elisp/net-fsm.el
+++ elisp/net-fsm.el
@@ -1,50 +1,47 @@
 ; Network state machine engine
 
-(eval-when-compile (require 'cl))
+(eval-when-compile
+  (require 'cl)
+  (or (fboundp 'defvar-local)
+      (defmacro defvar-local (var val &optional docstring)
+        (declare (debug defvar) (doc-string 3))
+        (list 'progn (list 'defvar var val docstring)
+              (list 'make-variable-buffer-local (list 'quote var))))))
 
 (defcustom fsm-use-debug-buffer nil
   "*Store fsm debug messages in a buffer."
   :type 'boolean
   :group 'distel)
 
-(defvar fsm-buffer-p nil
+(defvar-local fsm-buffer-p nil
   "Set to t in buffers belonging to FSMs, for sanity-checking.")
-(defvar fsm-state nil
-  "Current state.")
-(defvar fsm-process nil
+(defvar-local fsm-state nil "Current state.")
+(defvar-local fsm-process nil
   "Socket associated with this FSM.")
-(defvar fsm-cont nil
+(defvar-local fsm-cont nil
   "Continuation function called with the result of the FSM, if it
-  terminates successfully (with fsm-terminate).")
-(defvar fsm-fail-cont nil
+terminates successfully (with fsm-terminate).")
+(defvar-local fsm-fail-cont nil
   "Continuation function called with the result of the FSM, if it
-  terminates in failure.")
-(defvar fsm-work-buffer nil
+terminates in failure.")
+(defvar-local fsm-work-buffer nil
   "Buffer used for creating messages, dynamically bound in
-  `fsm-build-message'")
-(defvar fsm-put-data-in-buffer nil
+`fsm-build-message'.")
+(defvar-local fsm-put-data-in-buffer nil
   "When set to `t', new data is appended to the FSM's buffer in
 addition to being passed as an argument.")
-(defvar fsm-cleanup-hook nil)
 
-(make-variable-buffer-local 'fsm-buffer-p)
-(make-variable-buffer-local 'fsm-state)
-(make-variable-buffer-local 'fsm-process)
-(make-variable-buffer-local 'fsm-cont)
-(make-variable-buffer-local 'fsm-fail-cont)
-(make-variable-buffer-local 'fsm-work-buffer)
-(make-variable-buffer-local 'fsm-put-data-in-buffer)
+(defvar fsm-cleanup-hook nil)           ; unused
 
-(defmacro with-error-cleanup (cleanup &rest body)
+(defmacro fsm-with-error-cleanup (cleanup &rest body)
   "Execute BODY, and if it hits an error run CLEANUP."
+  (declare (indent 1))
   (let ((success (make-symbol "success")))
     `(let (,success)
        (unwind-protect
-	   (prog1 (progn ,@body)
-	     (setq ,success t))
-	 (unless ,success ,cleanup)))))
-
-(put 'with-error-cleanup 'lisp-indent-function 1)
+           (prog1 (progn ,@body)
+             (setq ,success t))
+         (unless ,success ,cleanup)))))
 
 ;; ----------------------------------------------------------------------
 ;; External API
@@ -52,12 +49,12 @@ addition to being passed as an argument.")
 
 (defun fsm-open-socket (host port)
   (let ((buf (generate-new-buffer " *net-fsm*")))
-    (with-error-cleanup (kill-buffer buf)
+    (fsm-with-error-cleanup (kill-buffer buf)
       (let ((p (open-network-stream "netfsm" buf host port)))
-	(set-process-coding-system p 'no-conversion 'no-conversion)
-	(if (fboundp 'set-process-filter-multibyte)
-	  (set-process-filter-multibyte p nil))
-	p))))
+        (set-process-coding-system p 'no-conversion 'no-conversion)
+        (when (fboundp 'set-process-filter-multibyte)
+          (set-process-filter-multibyte p nil))
+        p))))
 
 (defun fsm-connect (host port state0 &optional init-arg cont fail-cont buffer)
   "Connect to HOST on PORT and initialize a state machine in
@@ -67,7 +64,7 @@ INIT-ARG is passed to the state machine as the `init' event's
 argument. CONT is a function which is called with the FSM's result if
 it terminates successfully. FAIL-CONT is called with no arguments if
 the FSM fails."
-  (with-error-cleanup (funcall fail-cont)
+  (fsm-with-error-cleanup (funcall fail-cont)
     (let ((socket (fsm-open-socket host port)))
       (fsm-attach socket state0 init-arg cont fail-cont buffer))))
 
@@ -79,7 +76,7 @@ argument. CONT is a function which is called with the FSM's result if
 it terminates successfully. FAIL-CONT is called with no arguments if
 the FSM fails."
   (when buffer
-    (replace-process-buffer socket buffer))
+    (fsm-replace-process-buffer socket buffer))
   (with-current-buffer (process-buffer socket)
     (unless (featurep 'xemacs)
       (set-buffer-multibyte nil))
@@ -90,9 +87,9 @@ the FSM fails."
     (setq fsm-fail-cont fail-cont)
     (set-process-sentinel socket #'fsm-sentinel)
     (set-process-filter   socket #'fsm-filter)
-    (init-fsm init-arg)))
+    (fsm-init init-arg)))
 
-(defmacro with-fsm (fsm &rest body)
+(defmacro with-fsm (fsm &rest body)     ; unused
   "Execute BODY in the context (buffer) of FSM."
   `(with-current-buffer (process-buffer ,fsm)
      ,@body))
@@ -110,18 +107,18 @@ the FSM fails."
 
 (defun fsm-event (event &optional arg)
   "Process `event' in the current state."
-  (assert-fsm-invariants)
+  (fsm-assert-invariants)
   (fsm-debug "EVENT: %S - %S\n" event arg)
-  (with-error-cleanup
+  (fsm-with-error-cleanup
       (fsm-fail (format "Error on event %S in state %S"
-			event fsm-state))
-	(funcall fsm-state event arg)))
+                        event fsm-state))
+    (funcall fsm-state event arg)))
 
 (defun fsm-terminate (&optional result)
   "Terminate an FSM with success. The continuation function, if
 available, is called with RESULT."
   (fsm-debug "TERM : %S\n" result)
-  (assert-fsm-invariants)
+  (fsm-assert-invariants)
   (let ((cont fsm-cont))
     (fsm-shutdown)
     (when cont
@@ -150,12 +147,12 @@ available, is called with RESULT."
   "Print a debugging message to the *fsm-debug* buffer."
   (if fsm-use-debug-buffer
       (with-current-buffer (get-buffer-create "*fsm-debug*")
-	(unless (featurep 'xemacs)
-	  (set-buffer-multibyte nil))
-	(goto-char (point-max))
-	(insert (apply #'format (cons fmt (mapcar #'summarise args)))))))
+        (unless (featurep 'xemacs)
+          (set-buffer-multibyte nil))
+        (goto-char (point-max))
+        (insert (apply #'format (cons fmt (mapcar #'fsm-summarise args)))))))
 
-(defun check-event (event &rest allowed)
+(defun fsm-check-event (event &rest allowed)
   "Ensure that an event is allowed. If EVENT is not one of ALLOWED, an
 error is signaled."
   (unless (memq event allowed)
@@ -168,11 +165,12 @@ error is signaled."
 (defmacro fsm-build-message (&rest body)
   "Execute BODY, and return the message that it creates via calls to
 fsm-{insert,encode}*."
-  `(let ((fsm-work-buffer (let ((default-enable-multibyte-characters nil))
-			     (generate-new-buffer " *fsm-msg*"))))
+  `(let ((fsm-work-buffer (with-current-buffer (generate-new-buffer " *fsm-msg*")
+                            (set-buffer-multibyte nil)
+                            (current-buffer))))
      (unwind-protect
-	 (progn ,@body
-		(with-current-buffer fsm-work-buffer (buffer-string)))
+         (progn ,@body
+                (with-current-buffer fsm-work-buffer (buffer-string)))
        (kill-buffer fsm-work-buffer))))
 
 (defmacro fsm-with-message-buffer (&rest body)
@@ -180,7 +178,7 @@ fsm-{insert,encode}*."
 called outside fsm-build-message, BODY is just executed in the current
 buffer."
   `(with-current-buffer (or fsm-work-buffer
-			    (current-buffer)) ,@body))
+                            (current-buffer)) ,@body))
 
 (put 'fsm-build-message 'lisp-indent-function 'defun)
 (put 'fsm-with-message-buffer 'lisp-indent-function 1)
@@ -199,14 +197,14 @@ buffer."
   "Encode N as a 2-byte big-endian integer."
   (fsm-with-message-buffer
       (insert (logand (ash n -8) 255)
-	      (logand n          255))))
+              (logand n          255))))
 (defun fsm-encode4 (n)
   "Encode N as a 4-byte big-endian integer."
   (fsm-with-message-buffer
       (insert (logand (ash n -24) 255)
-	      (logand (ash n -16) 255)
-	      (logand (ash n -8)  255)
-	      (logand n           255))))
+              (logand (ash n -16) 255)
+              (logand (ash n -8)  255)
+              (logand n           255))))
 (defun fsm-insert (&rest args)
   "Insert ARGS (characters or strings) into the encoding buffer."
   (fsm-with-message-buffer
@@ -216,7 +214,7 @@ buffer."
 ;; Internals
 ;; ----------------------------------------------------------------------
 
-(defun init-fsm (init-arg)
+(defun fsm-init (init-arg)
   "Deliver initial events: INIT, and possibly DATA if some has arrived."
   (let ((data (buffer-string)))
     (erase-buffer)
@@ -245,26 +243,26 @@ buffer."
     (set-process-sentinel fsm-process nil)
     (kill-buffer (process-buffer fsm-process))))
 
-(defun assert-fsm-invariants ()
+(defun fsm-assert-invariants ()
   (assert fsm-buffer-p)
   (assert (not (null fsm-state))))
 
-(defun summarise (x)
+(defun fsm-summarise (x)
   (if (stringp x)
       (with-temp-buffer
-	(insert x)
-	(goto-char (point-min))
-	(while (search-forward "\n" nil t)
-	  (replace-match "\\n" nil t))
-	(elide-string (buffer-string) 30))
+        (insert x)
+        (goto-char (point-min))
+        (while (search-forward "\n" nil t)
+          (replace-match "\\n" nil t))
+        (fsm-elide-string (buffer-string) 30))
     x))
 
-(defun elide-string (s len)
+(defun fsm-elide-string (s len)
   (if (> (length s) len)
       (concat (substring s 0 (- len 3)) "...")
     s))
 
-(defun replace-process-buffer (process buffer)
+(defun fsm-replace-process-buffer (process buffer)
   (let ((oldbuffer (process-buffer process)))
     (set-process-buffer process buffer)
     (kill-buffer oldbuffer)))
